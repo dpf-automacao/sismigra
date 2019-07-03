d363063ee636bd40b6dc3dca22761f7631a1c5ce
Jefferson Fragoso De Souza <jfsouza2@stefanini.com>
2019-07-03 17:28:15 -0300

    Menu Consultar Imigrante

---
+++ features/specs/consultar_imigrante_spec.feature
@@ -0,0 +1,11 @@
+#language: pt
+
+Funcionalidade: Consultar Imigrante
+
+@consultar_imigrante
+Cenario: Consultar imigrante
+    Quando informar os dados para consultar o imigrante
+    E solicitar pesquisar
+    Entao deve apresentar registros correspodentes aos dados pesquisados
+
+
--- features/support/pages/menu_page.rb
+++ features/support/pages/menu_page.rb
@@ -1,30 +1,31 @@
 class MenuPage < SitePrism::Page
 
-    # Mapeamento de elementos de preenchimento
+  # Mapeamento de elementos de preenchimento
 
 
-    # Mapeamento de botoes, links
+  # Mapeamento de botoes, links
 
-    element :solicitacoes_menu, "div[id='j_id33:j_id34_span']"
-    element :processar_atendimento_submenu, "span[id='j_id33:j_id35:anchor']"
+  element :solicitacoes_menu, "div[id='j_id33:j_id34_span']"
+  element :processar_atendimento_submenu, "span[id='j_id33:j_id35:anchor']"
+  element :imigrante_menu, :xpath, '//div[text()="Imigrante"]'
+  element :consultar_imigrante_submenu, :xpath, '//span[text()="Consultar Imigrante"]'
 
-    # Mapeamento de elementos para validação
+  # Mapeamento de elementos para validação
 
 
-    # Definindo metodo para selecionar submenu processar atendimento
-
-    def processar_atendimento_sub_menu
-
-        if(has_solicitacoes_menu?)
-
-            solicitacoes_menu.hover
-            has_processar_atendimento_submenu?
-            processar_atendimento_submenu.click
-            
-
-        end
-
+  # Definindo metodo para selecionar submenu processar atendimento
+  def processar_atendimento_sub_menu
+    if(has_solicitacoes_menu?)
+      solicitacoes_menu.hover
+      has_processar_atendimento_submenu?
+      processar_atendimento_submenu.click
     end
+  end
+
+  def consultar_imigrante_submenu
+    imigrante_menu.hover
+    consultar_imigrante_submenu.click
+  end
 
 
 end
