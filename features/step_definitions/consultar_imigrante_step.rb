4ece2355cd8de66b8d8ff2f45da991ee135cf6ce
Jefferson Fragoso De Souza <jfsouza2@stefanini.com>
2019-07-04 16:02:23 -0300

    Esboço Consultar imigrante

---
+++ features/step_definitions/consultar_imigrante_step.rb
@@ -0,0 +1,10 @@
+Quando("informar os dados para consultar o imigrante") do
+
+end
+
+Quando("solicitar pesquisar") do
+
+end
+
+Entao("deve apresentar registros correspodentes aos dados pesquisados") do
+end
