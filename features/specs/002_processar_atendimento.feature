c64cadf602af0231c810b3f08b91a588a77b3781
Vinicius <vfcoutinho@stefanini.com>
2019-07-05 13:49:25 -0300

    Add: Processar Atendimento (ABERTO, ANALISE)

--- features/specs/002_processar_atendimento.feature
+++ features/specs/002_processar_atendimento.feature
@@ -2,6 +2,8 @@
 
 Funcionalidade: Processar Atendimento SISMIGRA
 
+# PROCESSAR ATENDIMENTO EM ABERTO
+
 @processar_atendimento_registro_aberto
 Cenario: Processar Atendimento de Solicitação de Registro em Aberto
     Quando for processar um atendimento de "Registro" em "Aberto"
@@ -60,6 +62,74 @@ Cenario: Processar Atendimento de Substituição de CRNM em Aberto
 @processar_atendimento_segunda_via_crnm_aberto
 Cenario: Processar Atendimento de Segunda via de CRNM em Aberto
     Quando for processar um atendimento de "Segunda via de CRNM" em "Aberto"
+    E preencho Dados Pessoais e clico em Proximo
+    E preencho Dados do Registro e clico em Proximo
+    E preencho Dados do Endereco e clico em Proximo
+    E preencho Dados de Documentos e clico em Proximo
+    E preencho Dados do Resultado da Pesquisa e clico em Proximo
+    E visualizo Dados da Previa da Carteira e clico em Concluir
+    Entao verifico o requerimento referente Em Processamento
+
+# PROCESSAR ATENDIMENTO EM ANALISE
+
+@processar_atendimento_registro_analise
+Cenario: Processar Atendimento de Registro em Análise
+    Quando for processar um atendimento de "Registro" em "Em análise"
+    E preencho Dados Pessoais e clico em Proximo
+    E preencho Dados do Registro e clico em Proximo
+    E preencho Dados do Endereco e clico em Proximo
+    E preencho Dados de Documentos e clico em Proximo
+    E preencho Dados do Resultado da Pesquisa e clico em Proximo
+    E visualizo Dados da Previa da Carteira e clico em Concluir
+    Entao verifico o requerimento referente Em Processamento
+
+@processar_atendimento_autorizacao_residencia_analise
+Cenario: Processar Atendimento de Solicitação de Autorização de Residencia em Análise
+    Quando for processar um atendimento de "Autorização de Residência" em "Em análise"
+    E preencho Dados Pessoais e clico em Proximo
+    E preencho Dados do Registro e clico em Proximo
+    E preencho Dados do Endereco e clico em Proximo
+    E preencho Dados de Documentos e clico em Proximo
+    E preencho Dados do Resultado da Pesquisa e clico em Proximo
+    E visualizo Dados da Previa da Carteira e clico em Concluir
+    Entao verifico o requerimento referente Em Processamento
+
+@processar_atendimento_alteracao_prazo_analise
+Cenario: Processar Atendimento de Alteração de Prazo em Análise
+    Quando for processar um atendimento de "Alteração de Prazo" em "Em análise"
+    E preencho Dados Pessoais e clico em Proximo
+    E preencho Dados do Registro e clico em Proximo
+    E preencho Dados do Endereco e clico em Proximo
+    E preencho Dados de Documentos e clico em Proximo
+    E preencho Dados do Resultado da Pesquisa e clico em Proximo
+    E visualizo Dados da Previa da Carteira e clico em Concluir
+    Entao verifico o requerimento referente Em Processamento
+
+@processar_atendimento_recadastramento_extemporaneo_analise
+Cenario: Processar Atendimento de Recadastramento Extemporâneo em Análise
+    Quando for processar um atendimento de "Recadastramento Extemporâneo" em "Em análise"
+    E preencho Dados Pessoais e clico em Proximo
+    E preencho Dados do Registro e clico em Proximo
+    E preencho Dados do Endereco e clico em Proximo
+    E preencho Dados de Documentos e clico em Proximo
+    E preencho Dados do Resultado da Pesquisa e clico em Proximo
+    E visualizo Dados da Previa da Carteira e clico em Concluir
+    Entao verifico o requerimento referente Em Processamento
+
+@processar_atendimento_substituicao_crnm_analise
+Cenario: Processar Atendimento de Substituição de CRNM em Análise
+    Quando for processar um atendimento de "Substituição de CRNM" em "Em análise"
+    E preencho Dados Pessoais e clico em Proximo
+    E preencho Dados do Registro e clico em Proximo
+    E preencho Dados do Endereco e clico em Proximo
+    E preencho Dados de Documentos e clico em Proximo
+    E preencho Dados do Resultado da Pesquisa e clico em Proximo
+    E visualizo Dados da Previa da Carteira e clico em Concluir
+    Entao verifico o requerimento referente Em Processamento
+
+@processar_atendimento_segunda_via_crnm_analise
+Cenario: Processar Atendimento de Segunda via de CRNM em Análise
+    Quando for processar um atendimento de "Segunda via de CRNM" em "Em análise"
     E preencho Dados Pessoais e clico em Proximo
     E preencho Dados do Registro e clico em Proximo
     E preencho Dados do Endereco e clico em Proximo
--- features/support/pages/processar_atendimento_page.rb
+++ features/support/pages/processar_atendimento_page.rb
@@ -15,14 +15,18 @@ class ProcessarAtendimentoPage < SitePrism::Page
     element :periodo_inicial_input, "input[id*='periodoInicioInputDate']"
     element :periodo_final_input, "input[id*='periodoFinalInputDate']"
     element :amparo_legal_input, "input[id*='inputText_comboAmparo']"
-    element :telefone_contanto_input, "input[id*='telefoneResidencial']"
+    element :amparo_legal_disabled_input, "input[id*='inputText_comboAmparo'][disabled='disabled']"
+    element :telefone_contato_input, "input[id*='telefoneResidencial']"
     element :resultado_da_pesquisa_textarea, "fieldset[class='mTop10px'] textarea[id*='resultadoPesquisa']"
+    element :justificativa_alteracoes_textarea, "table[id='modalJustificativaAlteracaoDadosPessoaisContentTable'] textarea"
+    element :justificativa_documentos_textarea, "table[id='modalJustificativaDocumentoContentTable'] textarea"
 
     # Mapeamento de botoes, links
 
     element :tipo_solicitacao_select, "select[id*='formulario-pesquisar:tipo']"
     element :situacao_requerimento_select, "select[id*='formulario-pesquisar:situacao']"
     element :pesquisar_requerimento_btn, "input[id*='botaoPesquisar']"
+    element :dependente_chamante, "input[id*='idDependente'][value='NAO']"
     element :proximo_btn, "input[value*='Próximo']"
     element :uf_select, "select[id*='uf']"
     element :municipio_select, "select[id*='Municipios']"
@@ -40,10 +44,23 @@ class ProcessarAtendimentoPage < SitePrism::Page
     element :salvar_btn, "input[value='Salvar']"
     element :concluir_btn, "input[value='Concluir']"
     element :encerrar_btn, "input[value='Encerrar']"
+    element :confirmar_alteracoes_btn, "table[id='modalJustificativaAlteracaoDadosPessoaisContentTable'] input[value*='Confirmar']"
+    element :confirmar_documentacao_btn, "table[id='modalJustificativaDocumentoContentTable'] input[value*='Confirmar']"
+
+    # Mapeamento das abas de Processar Atendimento
+
+    element :aba_dados_pessoais, "td[id*='DadosPessoais_lbl'][class*='dr-tbpnl-tb-act']"
+    element :aba_dados_registro, "td[id*='DadosRegistro_lbl'][class*='dr-tbpnl-tb-act']"
+    element :aba_dados_endereco, "td[id*='Endereco_lbl'][class*='dr-tbpnl-tb-act']"
+    element :aba_dados_documento, "td[id*='Documentos_lbl'][class*='dr-tbpnl-tb-act']"
+    element :aba_resultado_pesquisa, "td[id*='Pesquisa_lbl'][class*='dr-tbpnl-tb-act']"
+    element :aba_previa_carteira, "td[id*='PreviaCarteira_lbl'][class*='dr-tbpnl-tb-act']"
+
 
     # Mapeamento de elementos para validação e load
 
     element :carregamento_load, "img[src*='spinner.gif']"
+    element :dados_divergentes_erro, "dl[id='mensagens'] dt[class='mensagem_erro']"
     elements :mensagem_erro, "div.cRed"
 
     # Mapeamento de varios elementos
@@ -83,6 +100,7 @@ class ProcessarAtendimentoPage < SitePrism::Page
         @nr_requerimento = numeros_requerimentos[1].text
         gravar_dados("features/arquivos/requerimentos.txt", @nr_requerimento)
         btns_atendimento[1].click
+        wait_until_carregamento_load_invisible
 
     end
 
@@ -90,10 +108,31 @@ class ProcessarAtendimentoPage < SitePrism::Page
 
     def preencher_dados_pessoais
 
-        amparo_legal_input.set("36 - ART")
-        sleep(1)
-        amparo_legal_input.send_keys(:enter)
-        avancar_proximo_processar_atendimento
+        # SE ESTIVER NA ABA DADOS PESSOAIS
+
+        if(has_aba_dados_pessoais?(wait:1))
+
+            if(has_amparo_legal_disabled_input?(wait:1))
+
+                # SE ESTIVER DESABILITADO
+
+            else
+
+                amparo_legal_input.set("36 - ART")
+                sleep(1)
+                amparo_legal_input.send_keys(:enter)
+
+            end
+
+            if(has_dependente_chamante?(wait:1))
+
+                dependente_chamante.click
+
+            end
+
+            avancar_proximo_processar_atendimento
+
+        end
 
     end
 
@@ -101,73 +140,167 @@ class ProcessarAtendimentoPage < SitePrism::Page
 
     def preencher_dados_do_registro
 
-        uf_select.select("Acre")
-        municipio_select.select("Acrelândia")
-        avancar_proximo_processar_atendimento
+        # SE ESTIVER NA ABA DADOS DO REGISTRO
+
+        if(has_aba_dados_registro?(wait:1))
+
+            if(has_uf_select?(wait:1))
+
+                uf_select.select("Acre")
+                municipio_select.select("Acrelândia")
+
+            end
+
+            avancar_proximo_processar_atendimento
+
+        end
 
     end
+
+    # Definindo metodo para preencher Dados do Endereço
 
     def preencher_endereco
 
-        if(@tipo_solicitacao == "Autorização de Residência" && @situacao_requerimento == "Aberto")
+        # SE ESTIVER NA ABA DADOS DO ENDERECO
 
-            telefone_contanto_input.click.set("61999999999")
-            avancar_proximo_processar_atendimento
+        if(has_aba_dados_endereco?(wait:1))
 
-        else
+            if(@tipo_solicitacao == "Autorização de Residência" && @situacao_requerimento == "Aberto")
 
-            telefone_contanto_input.click.set("61999999999")
+                if(has_telefone_contato_input?(wait:1))
+
+                    telefone_contato_input.click.set("61999999999")
+
+                end
+
+            else
+
+                if(has_telefone_contato_input?(wait:1))
+
+                    telefone_contato_input.click.set("61999999999")
+
+                end
+
+            end
+
             avancar_proximo_processar_atendimento
 
         end
 
     end
+
+    # Definindo metodo para preencher Dados de Documentação
     
     def preencher_documentos
 
-        choose("Sim")
-        fotos.check
-        documento_de_viagem_checkbox.check
-        comprovante_filiacao.check
-        declaracao_endereco.check
-        certidao_nada_consta.check
-        comprovante_domicilio.check
-        adicionar_documento_btn.click
-        anexar(anexar_arquivo_btn(visible: false)["id"], "features/arquivos/arquivo_teste.jpg")
-        has_arquivos_anexados?
-        avancar_proximo_processar_atendimento
+        # SE ESTIVER NA ABA DOCUMENTOS
 
-    end
+        if(has_aba_dados_documento?(wait:1))
+
+            choose("Sim")
+
+            if(has_fotos?(wait:1))
+
+                fotos.check
+                adicionar_documento_btn.click
+
+            end
+
+            if(has_documento_de_viagem_checkbox?(wait:1))
+
+                documento_de_viagem_checkbox.check
+                adicionar_documento_btn.click
+
+            end
+
+            if(has_comprovante_filiacao?(wait:1))
+
+                comprovante_filiacao.check
+                adicionar_documento_btn.click
+
+            end
+
+            if(has_declaracao_endereco?(wait:1))
+
+                declaracao_endereco.check
+                adicionar_documento_btn.click
+
+            end
+
+            if(has_certidao_nada_consta?(wait:1))
+
+                certidao_nada_consta.check
+                adicionar_documento_btn.click
+
+            end
+
+            if(has_comprovante_domicilio?(wait:1))
+
+                comprovante_domicilio.check
+                adicionar_documento_btn.click
+
+            end
+
+            anexar(anexar_arquivo_btn(visible: false)["id"], "features/arquivos/arquivo_teste.jpg")
+            has_arquivos_anexados?
+
+            avancar_proximo_processar_atendimento
+
+            if(has_justificativa_documentos_textarea?(wait:1))
 
 
-    def preencher_resultado_da_pesquisa
+                justificativa_documentos_textarea.click.set("Justificando ausencia de documentacao")
+                has_confirmar_documentacao_btn?
+                confirmar_documentacao_btn.click
 
-        @indice = 0 
-        @tamanho_alterar_dados = alterar_dados_dos_registros.size
-
-        while(@indice < @tamanho_alterar_dados)
-
-            alterar_dados_dos_registros[@indice].click
-            resultado_da_pesquisa_textarea.set("NADA CONSTA")
-            salvar_btn.click
-            wait_until_carregamento_load_invisible
-            @indice += 1
+            end
 
         end
 
-        avancar_proximo_processar_atendimento
+    end
+
+    # Definindo metodo para preencher Resultados da Pesquisa
+
+    def preencher_resultado_da_pesquisa
+
+        # SE ESTIVER NA RESULTADO DA PESQUISA
+
+        if(has_aba_resultado_pesquisa?(wait:1))    
+
+            @indice = 0 
+            @tamanho_alterar_dados = alterar_dados_dos_registros.size
+
+            while(@indice < @tamanho_alterar_dados)
+
+                alterar_dados_dos_registros[@indice].click
+                resultado_da_pesquisa_textarea.set("NADA CONSTA")
+                salvar_btn.click
+                wait_until_carregamento_load_invisible
+                @indice += 1
+
+            end
+
+            avancar_proximo_processar_atendimento
+
+        end
 
     end
 
+    # Definindo metodo para Visualizar Previa da Carteira
+
     def visualizar_previa_carteira
 
-        binding.pry
-        has_concluir_btn?
-        concluir_btn.click
-        binding.pry
-        has_encerrar_btn?
-        page.assert_text('Dados salvos com sucesso')
-        encerrar_btn.click
+        # SE ESTIVER NA RESULTADO DE PREVIA DA CARTEIRA
+
+        if(has_aba_previa_carteira?(wait:1))
+
+            has_concluir_btn?
+            concluir_btn.click
+            has_encerrar_btn?
+            page.assert_text('Dados salvos com sucesso')
+            encerrar_btn.click
+
+        end
 
     end
 
@@ -178,23 +311,59 @@ class ProcessarAtendimentoPage < SitePrism::Page
         proximo_btn.click
         wait_until_carregamento_load_invisible
 
-
         if(has_associar_checkbox?(wait:1))
 
             associar_checkbox.click
             wait_until_confirmar_identidade_btn_disabled_invisible
+
             has_confirmar_identidade_btn?
             confirmar_identidade_btn.click
+            wait_until_carregamento_load_invisible
 
-            if(has_mensagem_erro?(wait:2))
+            sleep(1)
+
+            if(has_proximo_btn?(wait:1))
 
                 proximo_btn.click
+                wait_until_carregamento_load_invisible
 
             end
 
         end
 
-    end
+        if(has_mensagem_erro?(wait:3))
 
+            if(has_justificativa_alteracoes_textarea?(wait:1))
+
+                justificativa_alteracoes_textarea.set("Preenchendo justificativa de alteracao de dados do imigrante")
+                confirmar_alteracoes_btn.click
+                wait_until_carregamento_load_invisible
+
+            end
+
+            if(has_dados_divergentes_erro?(wait:1))
+
+                proximo_btn.click
+                wait_until_carregamento_load_invisible
+    
+            else
+    
+                proximo_btn.click
+                wait_until_carregamento_load_invisible
+    
+            end
+
+        end
+
+        if(has_dados_divergentes_erro?(wait:1))
+
+            proximo_btn.click
+            wait_until_carregamento_load_invisible
+
+        end
+
+        sleep(1)
+
+    end
 
 end 
