5bfedc595d472e7e5250bf0a6b3863c1fd62240f
Jefferson Fragoso De Souza <jfsouza2@stefanini.com>
2019-07-10 09:04:14 -0300

    Utilizando assert_text em detalhar imigrante

--- features/step_definitions/003_consultar_imigrante_step.rb
+++ features/step_definitions/003_consultar_imigrante_step.rb
@@ -10,10 +10,7 @@ Quando("informar os dados para pesquisa") do |table|
 end
 
 Entao("deve apresentar registros correspodentes aos dados pesquisados") do
-  expect(@consultar_imigrante_page).to have_text("MARISOL FERNANDA GALLEGOS LOPEZ")
-  expect(@consultar_imigrante_page).to have_text("G148129W")
-  expect(@consultar_imigrante_page).to have_text("01/01/2010")
-  expect(@consultar_imigrante_page).to have_text("ROSMERY LOPEZ FERNANDEZ")
+  @consultar_imigrante_page.validar_registro_encontrado
 end
 
 # @detalhar_imigrante
@@ -31,56 +28,28 @@ Quando("solicitar detalhar") do
 end
 
 Então("deve visualizar as informações dos dados pessoais") do
-  expect(@consultar_imigrante_page).to have_text(@consultar_imigrante_page.aba_dados_pessoais.text)
-  expect(@consultar_imigrante_page).to have_text("F028776Q") # Nº RNM
-  expect(@consultar_imigrante_page).to have_text("MAGDALENNYS NAZARETH MOYA GOLDING") # Nome
-  expect(@consultar_imigrante_page).to have_text("MARIA MAGDALENA MOYA GOLDING") # Mãe (Filiação 2)
-  expect(@consultar_imigrante_page).to have_text("Feminino") # Sexo
-  expect(@consultar_imigrante_page).to have_text("01/01/2000") # Data nascimento
-  expect(@consultar_imigrante_page).to have_text("MAGDALENNY55@GMAIL.COM") # Email
-  expect(@consultar_imigrante_page).to have_text("930 - ESTUDANTE") # Ocupação principal
-  expect(@consultar_imigrante_page).to have_text("COMANÁ") # Cidade de Nascimento
+  @consultar_imigrante_page.validar_dados_pessoais
 
   @consultar_imigrante_page.btn_proximo.click
   @consultar_imigrante_page.aguardar_carregamento
 end
 
 Então("deve visualizar as informações dos dados do registro") do
-  expect(@consultar_imigrante_page).to have_text(@consultar_imigrante_page.aba_dados_registro.text)
-  expect(@consultar_imigrante_page).to have_text("F028776Q")
-  expect(@consultar_imigrante_page).to have_text("MAGDALENNYS NAZARETH MOYA GOLDING")
-  expect(@consultar_imigrante_page).to have_text("Ativo") # Situação do registro
-  expect(@consultar_imigrante_page).to have_text("21/11/2018") # Data do registro
-  expect(@consultar_imigrante_page).to have_text("273 - PORTARIA INTERMINISTERIAL N 9/2018") # Amparo Legal
-  expect(@consultar_imigrante_page).to have_text("028776190481968502") # Nº da CIE
-  expect(@consultar_imigrante_page).to have_text("PACARAIMA") # Local de entrada
-  expect(@consultar_imigrante_page).to have_text("01/10/2018") # Data de entrada
+  @consultar_imigrante_page.validar_dados_registro
 
   @consultar_imigrante_page.btn_proximo.click
   @consultar_imigrante_page.aguardar_carregamento
 end
 
 Então("deve visualizar as informações do endereço") do
-  expect(@consultar_imigrante_page).to have_text(@consultar_imigrante_page.aba_endereco.text)
-  expect(@consultar_imigrante_page).to have_text("F028776Q")
-  expect(@consultar_imigrante_page).to have_text("MAGDALENNYS NAZARETH MOYA GOLDING")
-  expect(@consultar_imigrante_page).to have_text("DELEMIG/DREX/SR/PF/RR") # Unidade vinculada
-  expect(@consultar_imigrante_page).to have_text("69305-370") # CEP
-  expect(@consultar_imigrante_page).to have_text("Boa Vista") # Cidade
-  expect(@consultar_imigrante_page).to have_text("RR") # Sigla Estado
-  expect(@consultar_imigrante_page).to have_text("RUA FÁBIO MAGALHÃES") # Logradouro
-  expect(@consultar_imigrante_page).to have_text("TRINTA E UM DE MARÇO") # Bairro
+  @consultar_imigrante_page.validar_dados_endereco
 
   @consultar_imigrante_page.btn_proximo.click
   @consultar_imigrante_page.aguardar_carregamento
 end
 
 Então("deve visualizar as informações do histórico") do
-  expect(@consultar_imigrante_page).to have_text(@consultar_imigrante_page.aba_historico.text)
-  expect(@consultar_imigrante_page).to have_text("F028776Q")
-  expect(@consultar_imigrante_page).to have_text("MAGDALENNYS NAZARETH MOYA GOLDING")
-  expect(@consultar_imigrante_page).to have_text("Solicitações")
-  expect(@consultar_imigrante_page).to have_text("Histórico de manutenções de registro")
+  @consultar_imigrante_page.validar_dados_historico
 end
 
 # @reimprimir_protocolo
--- features/support/pages/003_consultar_imigrante_page.rb
+++ features/support/pages/003_consultar_imigrante_page.rb
@@ -1,5 +1,6 @@
 class ConsultarImigrantePage < PageHelper
 
+  element :nome_input, '#formulario-manter-estrangeiro\:nome'
   element :rnm_input, :xpath, '//*[text()[contains(.,"RNM:")]]/../input'
   element :data_nasc_input, '#formulario-manter-estrangeiro\:calDtNascInputDate'
   element :resultado_pesquisa, 'tbody[id*="dataModelEstrangeiro"] tr'
@@ -26,8 +27,8 @@ class ConsultarImigrantePage < PageHelper
   end
 
   def pesquisar_por_filtro(dados)
+    nome_input.set(dados[:valor]) if dados[:filtro] == 'nome'
     rnm_input.set(dados[:valor]) if dados[:filtro] == 'RNM'
-    data_nasc_input.set(dados[:valor]) if dados[:filtro] == 'data_nasc'
 
     btn_pesquisar.click
     aguardar_carregamento
@@ -41,4 +42,54 @@ class ConsultarImigrantePage < PageHelper
     aguardar_carregamento
   end
 
+  def validar_registro_encontrado
+    page.assert_text("MARISOL FERNANDA GALLEGOS LOPEZ")
+    page.assert_text("G148129W")
+    page.assert_text("01/01/2010")
+    page.assert_text("ROSMERY LOPEZ FERNANDEZ")
+  end
+
+  def validar_dados_pessoais
+    page.assert_text(aba_dados_pessoais.text) # Verifica se está na respectiva aba
+    page.assert_text("F028776Q") # Nº RNM
+    page.assert_text("MAGDALENNYS NAZARETH MOYA GOLDING") # Nome
+    page.assert_text("MARIA MAGDALENA MOYA GOLDING") # Mãe (Filiação 2)
+    page.assert_text("Feminino") # Sexo
+    page.assert_text("01/01/2000") # Data nascimento
+    page.assert_text("MAGDALENNY55@GMAIL.COM") # Email
+    page.assert_text("930 - ESTUDANTE") # Ocupação principal
+    page.assert_text("COMANÁ") # Cidade de Nascimento
+  end
+
+  def validar_dados_registro
+    page.assert_text(aba_dados_registro.text) # Verifica se está na respectiva aba
+    page.assert_text("F028776Q")
+    page.assert_text("MAGDALENNYS NAZARETH MOYA GOLDING")
+    page.assert_text("Ativo") # Situação do registro
+    page.assert_text("21/11/2018") # Data do registro
+    page.assert_text("273 - PORTARIA INTERMINISTERIAL N 9/2018") # Amparo Legal
+    page.assert_text("028776190481968502") # Nº da CIE
+    page.assert_text("PACARAIMA") # Local de entrada
+    page.assert_text("01/10/2018") # Data de entrada
+  end
+
+  def validar_dados_endereco
+    page.assert_text(aba_endereco.text) # Verifica se está na respectiva aba
+    page.assert_text("F028776Q")
+    page.assert_text("MAGDALENNYS NAZARETH MOYA GOLDING")
+    page.assert_text("DELEMIG/DREX/SR/PF/RR") # Unidade vinculada
+    page.assert_text("69305-370") # CEP
+    page.assert_text("Boa Vista") # Cidade
+    page.assert_text("RR") # Sigla Estado
+    page.assert_text("RUA FÁBIO MAGALHÃES") # Logradouro
+    page.assert_text("TRINTA E UM DE MARÇO") # Bairro
+  end
+
+  def validar_dados_historico
+    page.assert_text(aba_historico.text) # Verifica se está na respectiva aba
+    page.assert_text("F028776Q")
+    page.assert_text("MAGDALENNYS NAZARETH MOYA GOLDING")
+    page.assert_text("Solicitações")
+    page.assert_text("Histórico de manutenções de registro")
+  end
 end
